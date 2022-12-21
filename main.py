dbc21f96d7799f43a8cba0c69e5999d000b7921a
CarlesBDC <70140203+CarlesBDC@users.noreply.github.com>
2022-12-21 11:51:37 +0100

    New upload setup and plot

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ class Profile(object):
 
             # Moana necessary variables
             elif sensor == 'Moana':
-                d_moana_ini = {sn: os.listdir(self.path + 'logs/raw/Moana/' + sn) for sn in os.listdir(self.path + 'logs/raw/Moana/')}
+                d_moana_ini = {sn: os.listdir(self.path + 'logs/raw/Moana/' + sn) for sn in
+                               os.listdir(self.path + 'logs/raw/Moana/')}
 
             # Lowell necessary variable
             elif sensor == 'Lowell':
@@ -93,7 +94,8 @@ class Profile(object):
 
                 elif sensor == 'Moana':
                     with open(self.path + 'status.txt') as f_ble:
-                        d_moana = {sn: os.listdir(self.path + 'logs/raw/Moana/' + sn) for sn in os.listdir(self.path + 'logs/raw/Moana/')}
+                        d_moana = {sn: os.listdir(self.path + 'logs/raw/Moana/' + sn) for sn in
+                                   os.listdir(self.path + 'logs/raw/Moana/')}
                         l_rec_file = []
                         if len(d_moana_ini) != len(d_moana):
                             sn_news = [sn for sn in d_moana if sn not in d_moana_ini]
@@ -105,7 +107,8 @@ class Profile(object):
                             for sn in d_moana:
                                 if len(d_moana_ini[sn]) != len(d_moana[sn]):
                                     if '1' in f_ble.readline():
-                                        l_rec_file = [sn + '/' + file for file in d_moana[sn] if file not in d_moana_ini[sn]]
+                                        l_rec_file = [sn + '/' + file for file in d_moana[sn] if
+                                                      file not in d_moana_ini[sn]]
                                         l_rec_file.sort()
 
                         if len(l_rec_file) > 0:
@@ -135,8 +138,8 @@ class Profile(object):
                         gps.store_all_csv()  # necessary to store any gps data between the 10 minutes gps gap
                         self.connect_wireless(n_lowell, 'Lowell')  # sends raw data to BDC endpoint
                         ldata = Merge().merge(n_lowell, sensor, self.gear)  # merges sensor data and GPS
-                        
-                        #leMOLT = [self.add_eMOLT_header(e[0], e[1], sensor) for e in ldata]  # creates files with eMOLT format
+
+                        # leMOLT = [self.add_eMOLT_header(e[0], e[1], sensor) for e in ldata]  # creates files with eMOLT format
                         # self.eMOLT_cloud(leMOLT)  # sends merged data to eMOLT endpoint
                         self.cloud(ldata, sensor)  # sends merged data to BDC endpoint
                         print('waiting for the next profile...')
@@ -149,13 +152,16 @@ class Profile(object):
             conn_type = Connection().conn_type()
             if conn_type:  # wifi
                 print('There is internet connection')
-                Transfer('/home/ec2-user/rtd/vessels/{vess}/merged/{sensor}/'.format(
-                    vess=self.vessel_name, sensor=sensor)).upload(
-                    'merged/{sensor}/'.format(sensor=sensor) + filename, filename)
+                # Transfer('/home/ec2-user/rtd/vessels/{vess}/merged/{sensor}/'.format(
+                #     vess=self.vessel_name, sensor=sensor)).upload(
+                #     'merged/{sensor}/'.format(sensor=sensor) + filename, filename)
+                Transfer(self.path + 'merged/{sensor}/'.format(sensor=sensor) + filename,
+                         self.vessel_name + '/merged/' + sensor + '/' + filename)
                 print('Data transferred successfully to the AWS endpoint')
             else:
                 logging.debug('There is no network available')
-                print('There is no network available, merged data has not been uploaded, queued routine will try to upload the data later')
+                print(
+                    'There is no network available, merged data has not been uploaded, queued routine will try to upload the data later')
                 df.to_csv(self.path + 'queued/{sensor}/'.format(sensor=sensor) + filename, index=False)
             self.tow_num += 1
 
@@ -168,21 +174,24 @@ class Profile(object):
             gps_name = 'gps' + datetime.utcnow().strftime('%y%m%d') + '.csv'
             data_gps.to_csv(self.path + 'logs/gps/' + gps_name, index=False)
             try:
-                Transfer('/home/ec2-user/rtd/vessels/' + self.vessel_name + '/').upload('logs/gps/' + gps_name,
-                                                                                        'gps/' + gps_name)
+                # Transfer('/home/ec2-user/rtd/vessels/' + self.vessel_name + '/').upload('logs/gps/' + gps_name,
+                #                                                                         'gps/' + gps_name)
+                Transfer(self.path + 'logs/gps/' + gps_name, self.vessel_name + '/gps/' + gps_name)
             except paramiko.ssh_exception.SSHException:
                 logging.debug('GPS data was not uploaded')
                 print('GPS data was not uploaded')
 
             for file in l_rec_file:
-                Transfer('/home/ec2-user/rtd/vessels/' + self.vessel_name + '/').upload(
-                    'logs/raw/{sensor}/'.format(sensor=sensor) + file, 'sensor/{sensor}/'.format(sensor=sensor) + file.split('/')[-1])
+                # Transfer('/home/ec2-user/rtd/vessels/' + self.vessel_name + '/').upload(
+                #     'logs/raw/{sensor}/'.format(sensor=sensor) + file, 'sensor/{sensor}/'.format(sensor=sensor) + file.split('/')[-1])
+                Transfer(self.path + 'logs/raw/{sensor}/'.format(sensor=sensor) + file,
+                         self.vessel_name + '/raw/' + sensor + '/' + file.split('/')[-1])
 
     def dd2dms(self, dd):
-        mnt, sec = divmod(dd*3600, 60)
+        mnt, sec = divmod(dd * 3600, 60)
         deg, mnt = divmod(mnt, 60)
         mnts = '0' + str(int(mnt)) if len(str(int(mnt))) == 1 else str(int(mnt))
-        return str(int(deg)) + str(int(mnt)) + '.' + str(int(sec*1000))[:4]
+        return str(int(deg)) + str(int(mnt)) + '.' + str(int(sec * 1000))[:4]
 
     def satellite(self, dev, msg):
         device = self.list_ports(dev)
@@ -204,10 +213,10 @@ class Profile(object):
             time.sleep(1)
             file.close()
             print(filename.split('/')[-1], 'uploaded in eMOLT endpoint')
-        
+
     def add_eMOLT_header(self, filename, data, sensor):
         date_file = data['DATETIME'].iloc[-1]
-        #date_file = datetime.strptime(date_file, '%Y-%m-%d %H:%M:%S')
+        # date_file = datetime.strptime(date_file, '%Y-%m-%d %H:%M:%S')
         date_file = date_file.strftime('%Y%m%d_%H%M%S')
         logger_timerange_lim = setup_rtd.metadata['time_range']
         logger_pressure_lim = setup_rtd.metadata['Fathom'] * 1.8288  # convert from fathom to meter
@@ -219,17 +228,22 @@ class Profile(object):
         if sensor == 'Lowell':
             MAC_FILTER = [setup_rtd.metadata['mac_addr']]
             MAC_FILTER[0] = MAC_FILTER[0].lower()
-            new_filename = self.path + 'merged/eMOLT/{sensor}/'.format(sensor=sensor) + 'li_{SN}_{date}_{vessel}.csv'.format(SN=MAC_FILTER[0][-5:], date=date_file, vessel=vessel_name)
+            new_filename = self.path + 'merged/eMOLT/{sensor}/'.format(
+                sensor=sensor) + 'li_{SN}_{date}_{vessel}.csv'.format(SN=MAC_FILTER[0][-5:], date=date_file,
+                                                                      vessel=vessel_name)
         elif sensor == 'Moana':
             MAC_FILTER = [setup_rtd.parameters['moana_SN']]
-            new_filename = self.path + 'merged/eMOLT/{sensor}/'.format(sensor=sensor) + 'zt_{SN}_{date}_{vessel}.csv'.format(SN=MAC_FILTER[0][-5:], date=date_file, vessel=vessel_name)
+            new_filename = self.path + 'merged/eMOLT/{sensor}/'.format(
+                sensor=sensor) + 'zt_{SN}_{date}_{vessel}.csv'.format(SN=MAC_FILTER[0][-5:], date=date_file,
+                                                                      vessel=vessel_name)
         header_file = open(self.path + 'header.csv', 'w')
         header_file.writelines('Probe Type,{sensor}\nSerial Number,'.format(sensor=sensor) + MAC_FILTER[0][
-                                                                     -5:] + '\nVessel Number,' + vessel_num + '\nVessel Name,' + vessel_name + '\nDate Format,YYYY-MM-DD\nTime Format,HH24:MI:SS\nTemperature,C\nDepth,m\n')  # create header with logger number
+                                                                                             -5:] + '\nVessel Number,' + vessel_num + '\nVessel Name,' + vessel_name + '\nDate Format,YYYY-MM-DD\nTime Format,HH24:MI:SS\nTemperature,C\nDepth,m\n')  # create header with logger number
         header_file.close()
-        
+
         # AFTER GETTING THE TD DATA IN A DATAFRAME
-        data.rename(columns={'DATETIME': 'datet(GMT)', 'TEMPERATURE': 'Temperature (C)', 'PRESSURE': 'Depth (m)', 'LATITUDE': 'lat', 'LONGITUDE': 'lon'},
+        data.rename(columns={'DATETIME': 'datet(GMT)', 'TEMPERATURE': 'Temperature (C)', 'PRESSURE': 'Depth (m)',
+                             'LATITUDE': 'lat', 'LONGITUDE': 'lon'},
                     inplace=True)
 
         data['HEADING'] = 'DATA'  # add header DATA line
@@ -237,13 +251,16 @@ class Profile(object):
         data.index = data['HEADING']
         data = data[['datet(GMT)', 'lat', 'lon', 'Temperature (C)', 'Depth (m)']]
         data.to_csv(self.path + 'merged/{sensor}/{file}'.format(sensor=sensor, file=filename[:-4]) + '_S1.csv')
-        
-        os.system('cat ' + self.path + 'header.csv ' + self.path + 'merged/{sensor}/{file}_S1.csv > '.format(sensor=sensor, file=filename[:-4]) + new_filename)
+
+        os.system(
+            'cat ' + self.path + 'header.csv ' + self.path + 'merged/{sensor}/{file}_S1.csv > '.format(sensor=sensor,
+                                                                                                       file=filename[
+                                                                                                            :-4]) + new_filename)
         os.system('rm ' + self.path + 'merged/{sensor}/{file}_S1.csv'.format(sensor=sensor, file=filename[:-4]))
-        
+
         print('New file created as {file} to be sent to eMOLT endpoint'.format(file=new_filename))
         return new_filename, data
-    
+
     def list_ports(self, desc):
         list_usb = []
         if sys.platform.startswith('linux'):
@@ -265,7 +282,7 @@ while True:
         print("RPi started recording the main routine.\n")
         Profile(setup_rtd.parameters['sensor_type']).main()
     except:
-       print('Unexpected error:', sys.exc_info()[0])
-       time.sleep(60)
+      print('Unexpected error:', sys.exc_info()[0])
+      time.sleep(60)
 
 
--- sftp_aws.py
+++ sftp_aws.py
@@ -1,15 +1,13 @@
-import pysftp
+import boto3
+# import pysftp
 import config
 import setup_rtd
 
 class Transfer(object):
-	def __init__(self, path):
-		self.path = path
-		self.db = config.database_connection_details['development']
+	def __init__(self, local_path, remote_path):
+		self.metadata = config.s3_details['development']
+		self.s3 = boto3.client(self.metadata['service'],
+							   aws_access_key_id=self.metadata['access_key'],
+							   aws_secret_access_key=self.metadata['access_pwd'])
 
-	def upload(self, local_name, remote_name):
-		cnopts = pysftp.CnOpts()
-		cnopts.hostkeys = None
-		with pysftp.Connection(host=self.db['ssh_host'], username=self.db['ssh_username'], private_key=self.db['ssh_private_key'], cnopts=cnopts) as conn:
-			local_path, remote_path = setup_rtd.parameters['path'] + local_name, self.path + remote_name
-			conn.put(local_path, remote_path)
+		self.s3.upload_file(local_path, self.metadata['s3_bucket_name'], remote_path)
